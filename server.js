616c4f8393252fb428ea49fc2f338a2ecc56fe41
tom-aglow <tom@ohhhh.me>
2018-01-14 13:21:59 -0800

    bring eslint and editorconfig up to the project

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+app.listen(app.get('port'), () => {console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
