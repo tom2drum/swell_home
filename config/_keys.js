275b1bd6fb73297f76a9bf82fcb01879258ec52d
tom-aglow <tom@ohhhh.me>
2018-01-14 20:51:56 -0800

    add db seed and db connection config for prod

--- config/_keys.js
+++ config/_keys.js
@@ -3,6 +3,9 @@ switch (process.env.NODE_ENV) {
 	case 'test':
 		module.exports = require('./test');
 		break;
+	case 'production':
+		module.exports = require('./prod');
+		break;
 	default:
 		module.exports = require('./dev');
 }
