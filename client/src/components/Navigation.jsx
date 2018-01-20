8319df0166df274eccfdb31ec9f5c4272dbb64ca
tom-aglow <tom@ohhhh.me>
2018-01-19 17:21:42 -0800

    add layout and basic styles for navbar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => (
-		<nav>
-			<ul>
-				<li><Link to="/">Lookbook</Link></li>
-				<li><Link to="/shop">Shop</Link></li>
-				<li><Link to="/contact">Contact</Link></li>
-				<li><Link to="/about">About</Link></li>
-			</ul>
-		</nav>
-	)
+const overlayColors = {
+	"/": '#cdcdcd',
+	"/shop": '#e5554f',
+	"/contact": '#bfced6',
+	"/about": '#e8e0d3',
+};
+
+class Navigation extends Component {
+	render() {
+		return (
+			<div className="Navigation">
+				<nav className="navbar">
+					<ul className="links">
+						<li className="link"><Link to="/">Lookbook</Link></li>
+						<li className="link"><Link to="/shop">Shop</Link></li>
+						<li className="link"><Link to="/contact">Contact</Link></li>
+						<li className="link"><Link to="/about">About</Link></li>
+					</ul>
+					<ul className="social-media">
+						<li className="link"><a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><span className="fa fa-facebook" /></a></li>
+						<li className="link"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><span className="fa fa-twitter" /></a></li>
+						<li className="link"><a href="https://pintereest.com" target="_blank" rel="noopener noreferrer"><span className="fa fa-pinterest" /></a></li>
+						<li className="link"><a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><span className="fa fa-instagram" /></a></li>
+					</ul>
+				</nav>
+				<div className="overlay" />
+			</div>
+		)
+	}
+}
+
+export default Navigation;
