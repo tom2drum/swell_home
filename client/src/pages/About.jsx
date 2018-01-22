1eeabe66c49338953e10c280bf408ce817523646
tom-aglow <tom@ohhhh.me>
2018-01-21 20:05:14 -0800

    add page content

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import aboutImg from '../img/pages/about-mission.jpg';
 
 const About = () => {
 	return (
@@ -6,9 +7,30 @@ const About = () => {
 			<h1 className="page-heading">About</h1>
 			<section className="intro container">
 				<h2 className="page-subheading">Proudly designed for everyday living</h2>
-				<div className="holder" />
+				<div className="row">
+					<article className="col-lg-2">
+						<h3 className="article-heading">01 — Our Mission</h3>
+						<p>
+							Swell was born from the desire to make contemporary and functional design attainable
+							to everyone. We believe your home should make you proud whether enjoyed alone, or with
+							company. Meaningful design is not only how something looks but how you connect with it
+							in everyday living.
+						</p>
+					</article>
+					<div className="col-lg-2">
+						<img src={aboutImg} alt="swell home mission image" className="about-image" />
+						<div className="about-translation-box">
+							<p className="about-translation-name">
+								Swell. <span>/swɛl/</span>
+							</p>
+							<p className="about-translation-text">
+								Be intensely affected or filled with a particular emotion.
+							</p>
+						</div>
+					</div>
+				</div>
 			</section>
-			<div className="holder" />
+			{/*<div className="holder" />*/}
 		</main>
 	);
 };
